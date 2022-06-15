1a38ba0fad24f42d4c3740ba70f2509ddd23e739
Alex Jennings <aj0312@my.bristol.ac.uk>
2022-06-15 18:36:10 +0200

    req3 completed

--- src/App.jsx
+++ src/App.jsx
@@ -11,21 +11,37 @@ export default function App() {
     const register = async (e) => {
         e.preventDefault();
         // Write your register code here
-
-
-    };
+        fetch("http://localhost:4000/register", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(user),
+        })
+        .then((res) => res.json())
+        .then((userData) => {
+            console.log("what is this?: ", userData)
+            setRegisterResponse(`Thank you for registering, ${userData.data.username}!`);
+        });
+    }
 
     const login = async (e) => {
         e.preventDefault();
-        // Write your login code here
-
-        
+        // Write your register code here
+        fetch("http://localhost:4000/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(user),
+        })
+        .then((res) => res.json())
+        .then((userData) => {
+            console.log("what is this?: ", userData)
+            localStorage.setItem("token", `${userData.data}`)
+            setLoginResponse(`Thank you for logging-in, ${localStorage.getItem("token")}!`);
+        });
     };
-
-
-
-
-
 
     // You can safely ignore everything below this line, it's just boilerplate
     // so you can focus on the exercise requirements
